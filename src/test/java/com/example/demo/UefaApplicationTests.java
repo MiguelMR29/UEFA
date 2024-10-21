f39cbfa067c248b36bc346f5542142a46dcd0aec
MiguelMR29 <mirandaruiz968@gmail.com>
2024-10-21 11:46:22 -0500

    test

--- src/test/java/com/example/demo/UefaApplicationTests.java
+++ src/test/java/com/example/demo/UefaApplicationTests.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.uefa.UefaApplication;
 
 @SpringBootTest(classes = UefaApplication.class)
+@ComponentScan(basePackages = "com.uefa")
 class UefaApplicationTests {
 
 	@Test
